0ad233405463728a14086ae672c62cb38e712e01
Merissa Bridgeman <merissa.bridgeman98@gmail.com>
2021-02-01 12:16:44 -0800

    added coments

--- Playlist.py
+++ Playlist.py
@@ -4,79 +4,74 @@ class Playlist:
   def __init__(self):
     self.__first_song = None
 
-
-  # TODO: Create a method called add_song that creates a Song object and adds it to the playlist. This method has one parameter called title.
-
   def add_song(self, title):
     #create a new song object
-    new_song = Song(title)
+    #Make sure the song object is title cased
+    new_song = Song(title.title())
     #set the new song to the first song 
     new_song.next = self.__first_song
     #set the head to the new song
     self.__first_song = new_song
 
-
-
-  # TODO: Create a method called find_song that searches for whether a song exits in the playlist and returns its index. The method has one parameters, title, which is the title of the song to be searched for. If the song is found, return its index.
-
   def find_song(self, title):
     current_song = self.__first_song
-    counter = 0
+    index = 0
 
     while current_song != None:
+      #if we find the song title passes, return it's index
       if current_song.get_title() == title:
-        return counter
-      counter += 1
+        return index
+      # if we didn't find it, increase the index and move to the next song
+      index += 1
       current_song = current_song.next
+      #If we don't find the song in the list, return -1.
     return -1
 
-
-  # TODO: Create a method called remove_song that removes a song from the playlist. This method takes one parameter, title, which is the song that should be removed. 
-
+  #We need to keep track of the previous song and the current song
   def remove_song(self, title):
     current_song = self.__first_song
     previous_song = None
 
     while current_song.get_title() != None:
+      #if the song title does not equate to our title passed, move through the list
       if current_song.get_title() != title:
         previous_song = current_song
         current_song = current_song.next
-      # If the previous song points to the song I want to remove
-      # then set the pointer to the next next song
+
+      #if there is no previous song, we're on the first song so set it to None
       if current_song.get_title() == title:
         if previous_song == None:
           self.__first_song = None
           return
+          #if the next pointer is None then we're on the last song
+          # In that case set the previous song's next pointer to None
         if current_song.next == None:
           previous_song.next = None
           return
+          #If the song is the first or last song, set the previous song's next pointer
+          # to the current songs next pointer
         previous_song.next = current_song.next
         return
-
-
-
-  # TODO: Create a method called length, which returns the number of songs in the playlist.
 
   def length(self):
    counter = 0
    current_song = self.__first_song
-
+  # while there are songs in the list update the counter
    while current_song != None:
      counter +=  1
      current_song = current_song.next
     
    return counter
-  # TODO: Create a method called print_songs that prints a numbered list of the songs in the playlist.
-
-  # Example:
-  # 1. Song Title 1
-  # 2. Song Title 2
-  # 3. Song Title 3
 
   def print_songs(self):
+    #set the current song to the first song
     current_song= self.__first_song
+    #set the counter = 1
     counter = 1
+    # while there are songs in the list print the number it's on and the name
     while current_song != None:
       print (f"{counter}. {current_song}")
+      # set the song to the next song
       current_song = current_song.next
+      #increase the counter 
       counter += 1
